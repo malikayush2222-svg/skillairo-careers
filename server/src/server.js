12f568dc91bb3a7c47a0f816c2c9d299644f6119
Ayush <YOUR_GITHUB_EMAIL>
2026-09-07 19:13:56 +0530

    Define PORT for Render

--- server/src/server.js
+++ server/src/server.js
@@ -63,10 +63,11 @@ app.use('/api/auth', authRoutes);
 app.use('/api/jobs', jobRoutes);
 app.use('/api/applications', applicationRoutes);
 
-export default app;
-export { connectDB };
-
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+
+export default app;
+export { connectDB };
